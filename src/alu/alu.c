981fbede7572aff006ca7baa19e9ff1a080f8715
raeffu <raeffu@raeffu.ch>
2013-11-27 10:58:26 -0800

    started with alu impl

--- src/alu/alu.c
+++ src/alu/alu.c
@@ -1,5 +1,5 @@
-/* 
-   alu.c 
+/*
+   alu.c
    - 21.11.05/BHO1
    bho1 29.12.2006
    bho1 6.12.2007
@@ -9,8 +9,8 @@
    bho1 20.7.2011 - rewrite: minimize global vars, ALU-operations are modeled with fct taking in/out register as parameter
    bho1 6.11.2011 - rewrite flags: adding flags as functional parameter. Now alu is truly a function
    bho1 26.11.2012 - remove bit declaration from op_alu_asl and op_alu_ror as they are unused (this may change later)
-   
-   
+
+
    GPL applies
 
    -->> YOUR FULL NAME HERE  <<--
@@ -41,21 +41,24 @@ unsigned int c_in = 2;	/* carry in bit address */
 int zero_test(char accumulator[]){
   int i;
   for(i=0;accumulator[i]!='\0'; i++){
-	if(accumulator[i]!='0')		
+	if(accumulator[i]!='0')
 	  return 0;
   }
   return 1;
 }
 
 /*
-  Halfadder: addiert zwei character p,q und schreibt in 
+  Halfadder: addiert zwei character p,q und schreibt in
   den Mue-memory das summen-bit und das carry-bit.
 */
 void half_adder(char p, char q){
-
+  /* if(p=='0' && q=='0'){ */
+  /*   c */
+  /* } */
+  /* if(p=='1' && q=='1'){} */
 }
 
-/* 
+/*
    void adder(char pbit, char qbit, char cbit)
    Adder oder auch Fulladder:
    Nimmt zwei character bits und ein carry-character-bit
@@ -74,7 +77,11 @@ void full_adder(char pbit, char qbit, char cbit){
   Invertieren der Character Bits im Register reg
 */
 void one_complement(char reg[]){
+  int i;
 
+  for(i=0; reg[i] != '\0';i++){
+    reg[i] = (reg[i]=='0')?'1':'0';
+  }
 }
 
 /*
@@ -82,15 +89,16 @@ void one_complement(char reg[]){
   reg := K2(reg)
 */
 void two_complement(char reg[]){
-
+  //TODO
+  one_complement(reg);
 }
 
 
 /*
   Die Werte in Register rega und Register regb werden addiert, das
-  Resultat wird in Register accumulator geschrieben. Die Flags cflag, 
+  Resultat wird in Register accumulator geschrieben. Die Flags cflag,
   oflag, zflag und sflag werden entsprechend gesetzt
-  
+
   accumulator := rega + regb
 */
 void op_add(char rega[], char regb[], char accumulator[], char flags[]){
@@ -100,12 +108,12 @@ void op_add(char rega[], char regb[], char accumulator[], char flags[]){
 /*
 
   ALU_OP_ADD_WITH_CARRY
-  
+
   Die Werte des carry-Flags und der Register rega und
   Register regb werden addiert, das
-  Resultat wird in Register accumulator geschrieben. Die Flags cflag, 
+  Resultat wird in Register accumulator geschrieben. Die Flags cflag,
   oflag, zflag und sflag werden entsprechend gesetzt
-  
+
   accumulator := rega + regb + carry-flag
 
 */
@@ -115,9 +123,9 @@ void op_adc(char rega[], char regb[], char accumulator[], char flags[]){
 
 /*
   Die Werte in Register rega und Register regb werden subtrahiert, das
-  Resultat wird in Register accumulator geschrieben. Die Flags cflag, 
+  Resultat wird in Register accumulator geschrieben. Die Flags cflag,
   oflag, zflag und sflag werden entsprechend gesetzt
-  
+
   accumulator := rega - regb
 */
 void op_sub(char rega[], char regb[], char accumulator[], char flags[]){
@@ -135,9 +143,9 @@ void op_alu_sbc(char regina[], char reginb[], char regouta[], char flags[]){
 
 /*
   Die Werte in Register rega und Register regb werden logisch geANDet, das
-  Resultat wird in Register accumulator geschrieben. Die Flags cflag, 
+  Resultat wird in Register accumulator geschrieben. Die Flags cflag,
   oflag, zflag und sflag werden entsprechend gesetzt
-  
+
   accumulator := rega AND regb
 */
 void op_and(char rega[], char regb[], char accumulator[], char flags[]){
@@ -145,19 +153,19 @@ void op_and(char rega[], char regb[], char accumulator[], char flags[]){
 }
 /*
   Die Werte in Register rega und Register regb werden logisch geORt, das
-  Resultat wird in Register accumulator geschrieben. Die Flags cflag, 
+  Resultat wird in Register accumulator geschrieben. Die Flags cflag,
   oflag, zflag und sflag werden entsprechend gesetzt
-  
+
   accumulator := rega OR regb
 */
 void op_or(char rega[], char regb[], char accumulator[], char flags[]){
 
-} 
+}
 /*
   Die Werte in Register rega und Register regb werden logisch geXORt, das
-  Resultat wird in Register accumulator geschrieben. Die Flags cflag, 
+  Resultat wird in Register accumulator geschrieben. Die Flags cflag,
   oflag, zflag und sflag werden entsprechend gesetzt
-  
+
   accumulator := rega XOR regb
 */
 void op_xor(char rega[], char regb[], char accumulator[], char flags[]){
@@ -181,7 +189,7 @@ void op_not_b(char rega[], char regb[], char accumulator[], char flags[]){
 
 
 /*
-  Negation von Register rega 
+  Negation von Register rega
   rega := -rega
 */
 void op_neg_a(char rega[], char regb[], char accumulator[], char flags[]){
@@ -189,7 +197,7 @@ void op_neg_a(char rega[], char regb[], char accumulator[], char flags[]){
 }
 
 /*
-  Negation von Register regb 
+  Negation von Register regb
   regb := -regb
 */
 void op_neg_b(char rega[], char regb[], char accumulator[], char flags[]){
@@ -213,19 +221,19 @@ void op_alu_lsr(char regina[], char reginb[], char regouta[], char flags[]){
 
 }
 /*
-  rotate 
+  rotate
   rotate left
 */
 void op_alu_rol(char regina[], char reginb[], char regouta[], char flags[]){
-  
+
 }
 /*
-  rotate 
+  rotate
   rotate left
   Move each of the bits in  A one place to the right. Bit 7 is filled with the current value of the carry flag whilst the old bit 0 becomes the new carry flag value.
 */
 void op_alu_ror(char regina[], char reginb[], char regouta[], char flags[]){
-  
+
 }
 
 
@@ -234,7 +242,7 @@ void op_alu_ror(char regina[], char reginb[], char regouta[], char flags[]){
 */
 void alu_reset(){
   int i;
-  
+
   for(i=0;i<max_mue_memory;i++)
     m[i] = '0';
 }
@@ -244,12 +252,12 @@ void alu_reset(){
   Procedural approach to ALU with side-effect:
   Needed register are already alocated and may be modified
   mainly a switchboard
-  
+
   alu_fct(int opcode, char reg_in_a[], char reg_in_b[], char reg_out_accu[], char flags[])
 
 */
 void alu(unsigned int alu_opcode, char reg_in_a[], char reg_in_b[], char reg_out_accu[], char flags[]){
-  char dummyflags[9] = "00000000";  
+  char dummyflags[9] = "00000000";
   switch ( alu_opcode ){
   case ALU_OP_ADD :
     op_add(reg_in_a, reg_in_b, reg_out_accu, (flags==NULL)?dummyflags:flags);
@@ -290,16 +298,16 @@ void alu(unsigned int alu_opcode, char reg_in_a[], char reg_in_b[], char reg_out
   case ALU_OP_LSR :
     op_alu_lsr(reg_in_a, reg_in_b, reg_out_accu, (flags==NULL)?dummyflags:flags);
     break;
-  case ALU_OP_ROL: 
+  case ALU_OP_ROL:
     op_alu_rol(reg_in_a, reg_in_b, reg_out_accu, (flags==NULL)?dummyflags:flags);
     break;
-  case ALU_OP_ROR: 
+  case ALU_OP_ROR:
     op_alu_ror(reg_in_a, reg_in_b, reg_out_accu, (flags==NULL)?dummyflags:flags);
     break;
   case ALU_OP_RESET :
 	alu_reset();
 	break;
   default:
-    printf("ALU(%i): Invalide operation %i selected", alu_opcode, alu_opcode); 
-  }	
+    printf("ALU(%i): Invalide operation %i selected", alu_opcode, alu_opcode);
+  }
 }
---
+++ src/alu/temp.c
@@ -0,0 +1,10 @@
+twocomplement
+
+onecomplement(str)
+int i = 7;
+
+while(str[i] != '0'){
+str[i] = '0';
+i--;
+}
+str[i]='1';
